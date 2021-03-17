e6c39a5f5ca7c517112bc57a120c90cff32acfc8
amandasimonds <amandasimonds9@gmail.com>
2021-03-17 16:31:31 -0600

    loaded data into keylines chart

--- src/app/keylines/keylines.component.ts
+++ src/app/keylines/keylines.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import materials from "../../sample-data/materials.json";
+import { Component, OnInit } from '@angular/core';
+// import data from "../../sample-data/materials.json";
+import jsonData from "../../sample-data/parts.json";
 import KeyLines from 'keylines';
 
 @Component({
@@ -7,76 +8,70 @@ import KeyLines from 'keylines';
   templateUrl: './keylines.component.html',
   styleUrls: ['./keylines.component.scss']
 })
-export class KeylinesComponent {
+export class KeylinesComponent implements OnInit{
 
-  dataObj = materials;
-  materialsData : any[];
+  dataObj = jsonData;
 
-  chart : any;
+  parseJsonData() {
+    var jsonData = this.dataObj;
+    var chartItems = [];
+    for (var i = 0; i < jsonData.length; i++){
+      chartItems.push({
+        type: 'node',
+        id: jsonData[i].id,
+        c: 'rgb(255, 53, 27)',
+        t: jsonData[i].name
+      });
+      chartItems.push({
+        type: 'link',
+        id: jsonData[i].id,
+        id1: jsonData[i].category,
+        id2: '',
+      });
+    }
+    console.log(chartItems)
+    return chartItems;
+  }
+
+  generateRandomData(){
+    function randInt(begin, end) {
+      return Math.floor(Math.random() * (end - begin)) + begin;
+    }
+    var chartItems = [];
+    for (var i = 0; i < 100; i++) {
+      chartItems.push({
+        type: 'node',
+        id: 'item' + i,
+        c: 'rgb(255, 53, 27)',
+        t: 'item' + i
+      });
+      chartItems.push({
+        type: 'link',
+        id: 'link' + i,
+        id1: 'item' + i,
+        id2: 'item' + ((i > 50) ? randInt(0, 51) : randInt(51, 100))
+      });
+    }
+    return chartItems;
+  };
+
+  ngOnInit(): void {}
+
+  chart: KeyLines.Chart;
+
+  data: KeyLines.ChartData = {
+    type: 'LinkChart',
+    items: this.parseJsonData()
+  };
 
   klReady([chart]: [KeyLines.Chart]) {
     // save the chart reference
     this.chart = chart;
-
-    //parse the JSON data
-    this.parseJsonData();
+    console.log(this.data)
 
     // load the data into the chart
-    this.chart.load(this.materialsData);
+    this.chart.load(this.data);
     this.chart.layout();
   }
 
-  parseJsonData() {
-    let jsonData = this.dataObj;
-    let result = [];
-    for (var i = 0; i < jsonData.length; i++){
-      
-      console.log(jsonData[i]);
-    }
-  }
-
 }
-
-  // parseJsonData(obj) {
-  //   const nodes = obj.id.map((node) => ({
-  //       id: node.Id,
-  //       type: 'node'
-  //   }));
-  //   const links = obj.links.map((link) => ({
-  //       id: `l${link.Id}`,
-  //       type: 'link',
-  //       id1: link.Source,
-  //       id2: link.Target
-  //   }));
-
-  //   this.chart = {
-  //       type: 'LinkChart',
-  //       items: [...nodes, ...links]
-  //   };
-
-
-
-  // klChartReady(chart: KeyLines.Chart){
-  //   this.parseJSONData(this.materialsData, chart)
-  //   this.chart = chart;
-  // }
-
-  // klChartEvents(event: any) {
-  //   if (event.name === 'hover') {
-  //     console.log('Chart hover event triggered: ', event.args);
-  //   }
-  // }
-
-  // KeyLines.ChartData = {
-  //   type: 'LinkChart',
-  //   items: [ { type: 'node', id: 'ac1', u: 'assets/icons/bank.png', t: '45081063' },
-  //   { type: 'link', id: 'l10', id1: 'p2', id2: 'ac3', c: '#ff7f7f', w: 5 },
-  //   { type: 'link', id: 'l11', id1: 'p3', id2: 'ac3', c: '#ff7f7f', w: 5 },
-  //   { type: 'link', id: 'l12', id1: 'p3', id2: 'ad1', c: '#ff7f7f', w: 5 },
-  //   { type: 'link', id: 'l13', id1: 'p4', id2: 'ad2', c: '#ff7f7f', w: 5 },
-  //   { type: 'link', id: 'l14', id1: 'p4', id2: 'ac4', c: '#ff7f7f', w: 5 },
-  //   { type: 'link', id: 'l15', id1: 'p5', id2: 'ad3', c: '#ff7f7f', w: 5 },
-  //   { type: 'link', id: 'l16', id1: 'p5', id2: 'ac2', c: '#ff7f7f', w: 5 },
-  //   { type: 'link', id: 'l17', id1: 'p6', id2: 'ad3', c: '#ff7f7f', w: 5 },
-  //   { type: 'link', id: 'l18', id1: 'p6', id2: 'ac5', c: '#ff7f7f', w: 5 } ]
-  // };
